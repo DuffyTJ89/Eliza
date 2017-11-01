879200685428524d708823934eacf056c9fab1ce
DuffyTJ89 <tomduffy89@gmail.com>
2017-11-01 10:20:29 +0000

    added HTML for ELIZA input

--- ElizaHome.go
+++ ElizaHome.go
@@ -4,19 +4,19 @@
  package main
  
  import (
- 	"fmt"
  	"net/http"
      "log"
  )
  
  //function that prints the name to the web page
- func printName(w http.ResponseWriter, r *http.Request){
-     fmt.Fprintf(w, "Welcome to Eliza")
+ func server(w http.ResponseWriter, r *http.Request){
+	http.ServeFile(w, r, "ElizaChat.html")
  }//end printName
  
  func main() {
  	//handle requests by calling printName
-     http.HandleFunc("/", printName)
+	 http.HandleFunc("/", server)
+	 
  	//start webserver and serve on port 8080
      log.Println("Listening....")
      http.ListenAndServe(":8080", nil)
